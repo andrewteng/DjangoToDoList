2b6f7b626a9e10e921499bcb6eb56e64f732c484
ateng3 <ateng3@emory.edu>
2015-02-03 23:44:45 -0500

    updated user auth

--- todo/views.py
+++ todo/views.py
@@ -1,21 +1,25 @@
-from django.contrib.auth import logout
-from django.shortcuts import render_to_response  
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect, HttpResponse
-from todo.models import Item 
+from django.template import RequestContext, loader
+from django.contrib.auth.models import User
+
+from todo.models import Item
+from todo.forms import Form
 
 def login (request):
     if (request.method == 'POST'):
-        email = request.POST['email']
+        username = request.POST['username']
         password = request.POST['password']
-        user = authenticate (email = email, password = password)
+        user = authenticate (username = username, password = password)
         if user:
                 if user.is_active:
-                    login (request, user)
-                    return HttpResponseRedirect ('status_report.html')
-        else
-            return HttpResponse ("Email/Password not found.")
-    else
-        return render_to_response ('home.html')
+                    auth_login (request, user)
+                    return HttpResponseRedirect ('/report/') 
+        else:
+            return HttpResponse ("Username/Password combination not found.")
+    else:
+        return render(request, 'login.html', {})
 
 
 def logout(request):
@@ -23,5 +27,10 @@ def logout(request):
     return HttpResponseRedirect('/')
                               
 def status_report(request):  
-    todo_list = Item.objects.all()
-    return render_to_response('status_report.html', { 'todo_list': todo_list })
+
+    todo_listing = []  
+    for todo_list in Item.objects.all():  
+        todo_dict = {}  
+        todo_dict['list_object'] = todo_list.title  
+        todo_listing.append(todo_dict)  
+    return render_to_response('status_report.html', { 'todo_listing': todo_listing })
