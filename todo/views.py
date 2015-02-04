abc9ee69272aefe84b9ecd033e3a36124fd8f2b3
ateng3 <ateng3@emory.edu>
2015-02-03 23:43:28 -0500

    user authentication

--- todo/views.py
+++ todo/views.py
@@ -1,43 +1,27 @@
 from django.contrib.auth import logout
 from django.shortcuts import render_to_response  
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from todo.models import Item 
 
-from todo.models import List  
-from todo.models import Item  
+def login (request):
+    if (request.method == 'POST'):
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate (email = email, password = password)
+        if user:
+                if user.is_active:
+                    login (request, user)
+                    return HttpResponseRedirect ('status_report.html')
+        else
+            return HttpResponse ("Email/Password not found.")
+    else
+        return render_to_response ('home.html')
 
-'''
-def login(request):
-    user_obj = User.objects.filter(email=request.POST.get('email'), password=request.POST.get('password'))
-    if user_obj.count():
-        print (user_obj)
-        request.session['user_id']=user_obj[0].id
-        request.session['fname']=user_obj[0].fname
-    return HttpResponseRedirect('/')
-'''
 
-def logout_page(request):
-    """
-    Log users out and re-direct them to the main page.
-    """
+def logout(request):
     logout(request)
     return HttpResponseRedirect('/')
-
-def login(request):
-
-        return render_to_response('login.html')
-
-def addTask(request):
-
-    return render_to_response('addTask.html')
-
-def createAcct(request):
-    return render_to_response('createAcct.html')
                               
 def status_report(request):  
-
-    todo_listing = []  
-    for todo_list in Item.objects.all():  
-        todo_dict = {}  
-        todo_dict['list_object'] = todo_list.title  
-        todo_listing.append(todo_dict)  
-    return render_to_response('status_report.html', { 'todo_listing': todo_listing })
+    todo_list = Item.objects.all()
+    return render_to_response('status_report.html', { 'todo_list': todo_list })
