c77eccfa507fbb9ae93321b2644897631b70e135
Melissa Mendoza <mmendo3@dutch.mathcs.emory.edu>
2015-02-04 21:49:33 -0500

    modified:   ../db.sqlite3 	modified:   ../templates/status_report.html 	modified:   views.py

--- todo/views.py
+++ todo/views.py
@@ -26,11 +26,33 @@ def logout(request):
     logout(request)
     return HttpResponseRedirect('/')
                               
-def status_report(request):  
+#def status_report(request):  
+ #   todo_listing = []  
+  #  for todo_list in Item.objects.all():  
+   #     todo_dict = {}  
+    #    todo_dict['list_object'] = todo_list.title  
+     #   todo_listing.append(todo_dict)  
+   # return render_to_response('status_report.html', { 'todo_listing': todo_listing })
 
-    todo_listing = []  
-    for todo_list in Item.objects.all():  
-        todo_dict = {}  
-        todo_dict['list_object'] = todo_list.title  
-        todo_listing.append(todo_dict)  
-    return render_to_response('status_report.html', { 'todo_listing': todo_listing })
+def status_report(request): 
+    todo_list = Item.objects.all()
+    if(request.POST.get('Add')):
+        item = Item(item_text=request.POST.get('Task'))
+        item.save()
+    todo_list=Item.objects.all()
+    template= loader.get_template('status_report.html')
+    context = RequestContext(request, {
+        'todo_list': todo_list, 
+    })
+    return HttpResponse(template.render(context))
+
+def index(request):
+    todo_list= Item.objects.all()
+    context = {'todo_list':todo_list}
+    return render(request, 'status_report.html', context)
+
+def delete(request, item): 
+    item = Item.objects.get(item=item)
+    item.completed=True
+    item.delete()
+    return HttpResponse('/')
