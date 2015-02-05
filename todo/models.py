d68a8d579b5ab9efb7b5cf8472a4984fc6efc439
Melissa Mendoza <mmendo3@dutch.mathcs.emory.edu>
2015-02-05 15:25:10 -0500

    modified:   db.sqlite3 	modified:   templates/status_report.html 	modified:   todo/models.py

--- todo/models.py
+++ todo/models.py
@@ -5,12 +5,6 @@ from django.contrib.auth.models import User
 
 class Item(models.Model): 
   title = models.CharField(max_length=250) 
-<<<<<<< HEAD
-  created_date =  models.DateTimeField(default=datetime.datetime.now)
-  priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2) 
-=======
-  
->>>>>>> 0615f270a6687ed080a45a8b22ad2ba69faebe38
   completed = models.BooleanField(default=False) 
  
   def __str__(self): 
